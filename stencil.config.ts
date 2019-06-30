c4b11df2e44bc6d999d0f482824382b6bffff4d8
Stephen Coogan <hello@coog.ie>
2019-06-30 23:36:51 +0100

    feat: get Sassy

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,16 +1,18 @@
-import { Config } from '@stencil/core';
+import { Config } from "@stencil/core";
+import { sass } from "@stencil/sass";
 
 // https://stenciljs.com/docs/config
 
 export const config: Config = {
-  globalStyle: 'src/global/app.css',
-  globalScript: 'src/global/app.ts',
+  globalStyle: "src/global/app.css",
+  globalScript: "src/global/app.ts",
+  plugins: [sass()],
   outputTargets: [
     {
-      type: 'www',
+      type: "www",
       // comment the following line to disable service workers in production
       serviceWorker: null,
-      baseUrl: 'https://myapp.local/'
+      baseUrl: "https://myapp.local/"
     }
   ]
 };
