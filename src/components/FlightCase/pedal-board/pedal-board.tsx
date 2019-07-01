6497e4b2b000bd7c6e194e085a57c35cfef672fa
Stephen Coogan <hello@coog.ie>
2019-07-01 01:00:42 +0100

    feat: get audio element into pedal-board

--- src/components/FlightCase/pedal-board/pedal-board.tsx
+++ src/components/FlightCase/pedal-board/pedal-board.tsx
@@ -1,4 +1,6 @@
-import { Component, h } from "@stencil/core";
+import { Component, h, Element, State } from "@stencil/core";
+
+import { AudioContextManager } from "../../../global/AudioContextManager.js";
 
 @Component({
   tag: "pedal-board",
@@ -6,6 +8,25 @@ import { Component, h } from "@stencil/core";
   shadow: true
 })
 export class PedalBoard {
+  @Element() el;
+  @State() playing: boolean = false;
+
+  audioNode;
+
+  componentWillLoad() {
+    const ctx = AudioContextManager.context;
+    const audioTag = document.createElement("audio");
+    audioTag.src = "assets/audio/TombComb.mp3";
+    this.audioNode = audioTag;
+
+    this.el.appendChild(audioTag);
+    AudioContextManager.source = ctx.createMediaElementSource(audioTag);
+  }
+
+  componentDidLoad() {
+    AudioContextManager.sealPedalBoard();
+  }
+
   render() {
     return (
       <div class="pedal-board">
