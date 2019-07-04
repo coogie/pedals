9d71a2a78e74af207abef626e9372839bf2edafb
Stephen Coogan <hello@coog.ie>
2019-07-04 22:01:46 +0100

    feat(board): allow selecting multiple audio files

--- src/components/FlightCase/pedal-board/pedal-board.tsx
+++ src/components/FlightCase/pedal-board/pedal-board.tsx
@@ -11,27 +11,59 @@ export class PedalBoard {
   @Element() el;
   @State() playing: boolean = false;
 
+  demoFileNodes = [];
   audioNode;
 
   componentWillLoad() {
-    const ctx = AudioContextManager.context;
-    const audioTag = document.createElement("audio");
-    audioTag.src = "assets/audio/OrangeDrive.mp3";
-    audioTag.loop = true;
-    this.audioNode = audioTag;
+    const files = [
+      "FloatingRotary",
+      "NoFear",
+      "OrangeDrive",
+      "TheSeventies",
+      "TombComb",
+      "VintageCombo"
+    ];
 
-    this.el.appendChild(audioTag);
-    AudioContextManager.source = ctx.createMediaElementSource(audioTag);
+    const frag = document.createDocumentFragment();
+    for (let filename of files) {
+      const tag = document.createElement("audio");
+      tag.src = `assets/audio/${filename}.mp3`;
+      tag.id = filename;
+      tag.loop = true;
+      this.demoFileNodes.push(tag);
+      AudioContextManager.sources.push(tag);
+    }
+    this.el.appendChild(frag);
+
+    this.setActiveAudioNode(0);
+  }
+
+  setActiveAudioNode(index) {
+    this.togglePlaying("pause");
+    this.audioNode = this.demoFileNodes[index];
   }
 
   componentDidLoad() {
     AudioContextManager.sealPedalBoard();
   }
 
-  togglePlaying() {
+  togglePlaying(override = null) {
+    if (!this.audioNode) return;
+
     const ctx = AudioContextManager.context;
     if (ctx.state === "suspended") {
       ctx.resume();
+    }
+
+    if (override) {
+      if (override === "pause") {
+        this.audioNode.pause();
+        this.playing = true;
+      }
+      if (override === "play") {
+        this.audioNode.play();
+        this.playing = true;
+      }
     }
 
     if (!this.playing) {
@@ -46,12 +78,18 @@ export class PedalBoard {
   render() {
     return (
       <div class="PedalBoard">
-        <h1>
-          Pedals
-          <button onClick={() => this.togglePlaying()}>
-            {this.playing ? "◼" : "►"}
-          </button>
-        </h1>
+        <select
+          onChange={e =>
+            this.setActiveAudioNode((e.target as HTMLSelectElement).value)
+          }
+        >
+          {this.demoFileNodes.map((node, index) => (
+            <option value={index}>{node.id}</option>
+          ))}
+        </select>
+        <button onClick={() => this.togglePlaying()}>
+          {this.playing ? "◼" : "►"}
+        </button>
 
         <div class="PedalBoard__pedals">
           <pedal-boost />
